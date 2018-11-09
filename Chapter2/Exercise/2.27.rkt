b229393e37c1024c50193188c8d59882e63ddd1c
PillVic <1093332977@qq.com>
2018-11-09 09:42:12 +0800

    done

--- Chapter2/Exercise/2.27.rkt
+++ Chapter2/Exercise/2.27.rkt
@@ -7,15 +7,26 @@
         (iter (cdr l) (cons (car l) t))))
   (iter l nil))
 
-(define deep-reverse
-  (lambda (t)
-    (if (
+(define (deep-reverse t)
+  (define (iter sub r)
+    (if [null? sub]
+        r
+        (iter (cdr sub)
+              (cons (if [pair? (car sub)]
+                        (deep-reverse (car sub))
+                        (car sub))
+                    r))))
+  (iter t nil))
 
 ;;;;;;test
 (define x (list (list 1 2) (list 3 4)))
 
-(display (deep-reverse x))
+(display x)
 
 (newline)
 
-(display (list (list 4 3) (list 2 1)))
+(display (reverse x))
+
+(newline)
+
+(display (deep-reverse x))
