2690dd976bf0fe38dbfee18944595ca6faacc739
PillVic <benlmf@qq.com>
2019-01-02 11:03:58 +0800

    2.46 done

---
+++ Chapter2/Exercise/2.46.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define (make-vect x y)
+  (cons x y))
+
+(define (xcor-vect v)
+  (car v))
+
+(define (ycor-vect v)
+  (cdr v))
+
+(define (add-vect v1 v2)
+  (make-vect (+ (xcor-vect v1)
+                (xcor-vect v2))
+             (+ (ycor-vect v1)
+                (ycor-vect v2))))
+
+(define (sub-vect v1 v2)
+  (make-vect (- (xcor-vect v1)
+                (xcor-vect v2))
+             (- (ycor-vect v1)
+                (ycor-vect v2))))
+
+(define (scale-vect s v)
+  (make-vect (* s (xcor-vect v))
+             (* s (ycor-vect v))))
+
+;;;test
+
+(define v1 (make-vect 1 2))
+(define v2 (scale-vect 3 v1))
+(display (add-vect v1 v2))
+(display (sub-vect v1 v2))
